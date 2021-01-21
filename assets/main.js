bda64c181e5bc6419c5a0cce64f53177182f2f40
jojahn <jjahns.mail@gmail.com>
2021-01-21 15:28:14 +0100

    updated

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,16 @@
 function onContact() {
     window.location.href = "https://github.com/jojahn";
 }
+
+function appendStylesheet(file) {
+    var link = document.createElement("link");
+    link.href = "/assets/" + file + ".css";
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.media = "screen,print";
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // appendStylesheet("dark");
+}
